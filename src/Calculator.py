369fbe3efb3aa105b2dc2ddd086703ffb2a33c25
sainarasimharao <srao@tapestry.com>
2021-07-06 08:15:04 +0530

    Adding Addition and Subtraction Test cases

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,25 +1,21 @@
-from CsvReader import CsvReader
+import math
 
 
 def addition(a, b):
-    c = a + b
+    a = int(a)
+    b = int(b)
+    c = a+b
     return c
 
 
 def subtraction(a, b):
     a = int(a)
     b = int(b)
-    c = b - a
+    c = a - b
     return c
 
 
-def mean(data):
-    mean = data
-    return mean
-
-
 class Calculator:
-    result = 0
 
     def __init__(self):
         pass
@@ -31,14 +27,3 @@ class Calculator:
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
-
-
-class CSVStats(Calculator):
-    data = []
-
-    def __init__(self, data_file):
-        self.data = CsvReader(data_file)
-        pass
-
-    def mean(self):
-        mean(self.data)
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -5,20 +5,25 @@ from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
+
     def setUp(self) -> None:
         self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_subtraction(self):
-        test_data = CsvReader('/src/subtraction.csv').data
-        for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
+    def test_addition(self):
+        test_Data = CsvReader('/src/Test_Addition.csv').data
+        for row in test_Data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']),int(row['Result']))
 
-    def test_results_property(self):
-        self.assertEqual(self.calculator.result, 0)
+    def test_subtraction(self):
+        test_Data = CsvReader('/src/Test_Subtract.csv').data
+        for row1 in test_Data:
+            self.assertEqual(self.calculator.subtract(row1['Value 1'], row1['Value 2']),int(row1['Result']))
+
+def test_results_property(self):
+    self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
